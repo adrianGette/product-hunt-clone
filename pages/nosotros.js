46c379f2d2f8c4c031b8b728458615dae1470bfb
Adrian Gette <adrian.gette@icloud.com>
2021-08-03 16:42:08 -0300

    Probar Routing en nextjs

---
+++ pages/nosotros.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//import styled from '@emotion/styled';
+import Layout from '../components/layout/Layout';
+
+const Nosotros = () => (
+  <div>
+    <Layout>
+      <h1>Nosotros</h1>
+    </Layout>
+  </div>
+)
+
+export default Nosotros
+
