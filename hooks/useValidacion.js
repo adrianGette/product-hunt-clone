051412c832b9880a98b63e63c3d9acac0eb1b74b
Adrian Gette <adrian.gette@icloud.com>
2021-08-04 15:34:30 -0300

    Crear Hook para validar formularios

---
+++ hooks/useValidacion.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+
+const useValidacion = (stateInicial, validar, fn) => {
+
+    const [ valores, guardarValores ] = useState(stateInicial);
+    const [ errores, guardarErrores ] = useState({});
+    const [ submitForm, guardarSubmitForm ] = useState(false);
+
+    useEffect( () => {
+        if(submitForm) {
+            // Object.keys para revisar si el objeto tiene contenido
+            const noErrores = Object.keys(errores).length === 0;
+
+            if(noErrores) {
+                fn(); // función que se ejecuta en el componente
+            }
+            guardarSubmitForm(false);
+        }
+    }, []);
+
+    return ();
+}
+
+export default useValidacion;
