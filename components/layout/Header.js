ccfdab169973279d1b95af227df14f79c3ccacd9
Adrian Gette <adrian.gette@icloud.com>
2021-08-04 13:19:52 -0300

    CSS al Header

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -2,13 +2,42 @@ import React from 'react';
 import Buscar from '../ui/Buscar';
 import Navegacion from './Navegacion';
 import Link from 'next/link';
+import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+
+const ContenedorHeader = styled.div`
+    max-width: 1200px;
+    width: 95%;
+    margin: 0 auto;
+
+    @media (min-width: 768px) {
+        display: flex;
+        justify-content: space-between;
+    }
+`;
+
+const Logo = styled.p`
+    color: var(--naranja);
+    font-size: 4rem;
+    line-height: 0;
+    font-weight: 700;
+    font-family: 'Roboto Slab', serif;
+    margin-right: 2rem;
+`;
 
 const Header = () => {
     return (
-        <header>
-            <div>
+        <header
+            css={css`
+                border-bottom: 2px solid var(--gris3);
+                padding: 1rem 0;
+            `}
+        >
+            <ContenedorHeader>
                 <div>
-                    <p>P</p>
+                    <Link href="/">
+                        <Logo>P</Logo>
+                    </Link>
 
                     <Buscar />
 
@@ -22,7 +51,7 @@ const Header = () => {
                     <Link href="/">Login</Link>
                     <Link href="/">Crear Cuenta</Link>
                 </div>
-            </div>
+            </ContenedorHeader>
         </header>
     );
 }
