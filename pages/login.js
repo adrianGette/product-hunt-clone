4d1e5a1a82f1fc09282a837ed9d537dc30230b1a
Adrian Gette <adrian.gette@icloud.com>
2021-08-05 16:05:34 -0300

    Crear página de inicio de sesión

--- pages/login.js
+++ pages/login.js
@@ -1,13 +1,89 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { css } from '@emotion/react';
+import Router from 'next/router';
 import Layout from '../components/layout/Layout';
+import { Formulario, Campo, InputSubmit, Error } from '../components/ui/Formulario';
 
-const Login = () => (
-  <div>
-    <Layout>
-      <h1>Login</h1>
-    </Layout>
-  </div>
-)
+import firebase from '../firebase';
+
+// validaciones
+import useValidacion from '../hooks/useValidacion';
+import validarIniciarSesion from '../validacion/validarIniciarSesion';
+
+const STATE_INICIAL = {
+  email: '',
+  password: ''
+}
+
+const Login = () => {
+
+  const [ error, guardarError ] = useState(false);
+
+  const { valores, errores, handleSubmit, handleChange, handleBlur } = useValidacion(STATE_INICIAL, validarIniciarSesion, iniciarSesion);
+
+  const { email, password } = valores;
+
+  function iniciarSesion() {
+    console.log("Iniciando sesion . . .");
+  }
+
+  return (
+    <div>
+      <Layout>
+        <>
+          <h1
+            css={css`
+              text-align: center;
+              margin-top: 5rem;
+            `}
+          >Iniciar Sesión</h1>
+          <Formulario
+            onSubmit={handleSubmit}
+          >
+            
+  
+            <Campo>
+              <label htmlFor="email">Email</label>
+              <input 
+                type="email"
+                id="email"
+                placeholder="Tu Email"
+                name="email"
+                value={email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+            </Campo>
+
+            { errores.email && <Error>{errores.email}</Error> }
+  
+            <Campo>
+              <label htmlFor="password">Password</label>
+              <input 
+                type="password"
+                id="password"
+                placeholder="Tu Password"
+                name="password"
+                value={password}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+            </Campo>
+
+            { errores.password && <Error>{errores.password}</Error> }
+
+            { error && <Error>{error}</Error> }
+  
+            <InputSubmit 
+              type="submit"
+              value="Iniciar Sesión"
+            />
+          </Formulario>
+        </>
+      </Layout>
+    </div>
+  )
+}
 
 export default Login
 
