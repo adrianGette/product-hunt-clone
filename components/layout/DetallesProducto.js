d14f2045a6076ffda33f6ef932f3b361f95d4a57
Adrian Gette <adrian.gette@icloud.com>
2021-08-07 16:17:52 -0300

    Crear el componente de Producto

---
+++ components/layout/DetallesProducto.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const DetallesProducto = ({producto}) => {
+
+    const {
+        id,
+        comentarios,
+        creado,
+        descripcion,
+        empresa,
+        nombre,
+        url,
+        urlimagen,
+        votos
+    } = producto;
+
+    return (
+        <li>
+            <div>
+                <div>
+
+                </div>
+
+                <div>
+                    <h1>{nombre}</h1>
+                </div>
+            </div>
+
+            <div>
+
+            </div>
+        </li>
+    );
+}
+
+export default DetallesProducto;
