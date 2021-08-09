129325027f5d36baffc49d66f4e422f713e74110
Adrian Gette <adrian.gette@icloud.com>
2021-08-09 20:34:17 -0300

    Routing dinámico en Next en DetallesProducto

--- components/layout/DetallesProducto.js
+++ components/layout/DetallesProducto.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from '@emotion/styled';
 import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 import { es } from 'date-fns/locale';
+import Link from 'next/link';
 
 const Producto = styled.li`
     padding: 4rem;
@@ -107,7 +108,9 @@ const DetallesProducto = ({producto}) => {
                 </div>
 
                 <div>
-                    <Titulo>{nombre}</Titulo>
+                    <Link href="/productos/[id]" as={`/productos/${id}`}>
+                        <Titulo>{nombre}</Titulo>
+                    </Link>
                     <TextoDescripcion>{descripcion}</TextoDescripcion>
 
                     <Comentarios>
