2950c6dda2e5d4138d2d2eee43a45f4eb1584881
Adrian Gette <adrian.gette@icloud.com>
2021-08-10 14:24:08 -0300

    Editar [id].js y obtener el id del producto

--- pages/productos/[id].js
+++ pages/productos/[id].js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 const Producto = () => {
-    return ( <h1>Desde [id],js</h1> );
+
+    // routing para obtener el id actual
+    const router = useRouter();
+    const { query: { id } } = router;
+
+    useEffect(() => {
+        if(id) {
+            console.log('Ya hay un id ', id);
+        }
+    }, [id]);
+
+    return ( <h1>{id}</h1> );
 }
 
 export default Producto;
