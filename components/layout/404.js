00e00b8170abcfdb673b4e0450301757086a6b84
Adrian Gette <adrian.gette@icloud.com>
2021-08-17 14:24:56 -0300

    Validar que un producto exista

---
+++ components/layout/404.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { css } from '@emotion/react';
+
+const Error404 = () => {
+    return (
+        <h1
+            css={css`
+                margin-top: 5rem;
+                text-align: center;
+            `}
+        >Producto inexistente.</h1>
+    );
+}
+
+export default Error404;
